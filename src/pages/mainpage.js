4a21df2813a7d9ef833c6f3a5207e32075092726
procheta1999 <bhattacharyya.procheta1999@gmail.com>
2021-05-09 03:00:14 +0530

    chart left

--- src/pages/mainpage.js
+++ src/pages/mainpage.js
@@ -1,20 +1,23 @@
 import React ,{useState} from 'react';
+import { Suspense } from 'react';
 import "antd/dist/antd.css";
-import InputPage from './components/input';
-import Show from './components/table';
+import {LoadingOutlined} from '@ant-design/icons';
+// import InputPage from './components/input';
+// import Show from './components/table';
 import { Card, Tooltip } from 'antd';
 
-
-
+const InputPage = React.lazy(() => import('./components/input'));
+const Show = React.lazy(() => import('./components/table'));
 const MainPage = props => {
     const { query, history } = props;
     return (
-      <div>
+      <Suspense fallback={<div style={{marginTop:'300px', marginLeft:'700px'}}><LoadingOutlined style={{color:'blue',fontSize:'100px'}}/></div>}>
+        
         <InputPage history={history} />
         <br></br>
         <Show/>
         
-      </div>
+     </Suspense>
     );
   };
   export default MainPage
