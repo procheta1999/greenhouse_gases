ddd3485f19cb089e3611a709051f5f8911f455e7
procheta1999 <bhattacharyya.procheta1999@gmail.com>
2021-05-09 16:34:53 +0530

    chart doing

--- src/pages/components/chart.js
+++ src/pages/components/chart.js
@@ -1,20 +1,35 @@
 import React , {useState} from 'react';
+import {useEffect} from 'react';
 import data from '../output.json';
 import { Row, Col, Card, Select } from 'antd';
 const { Option } = Select;
 
 const Graph=()=>{
     const [countryList,setcountryList]=useState([]);
-    var a=[];
+    const [categoryList,setcategoryList]=useState([]);
     const country =()=>{
+        var a=[];
+        var b=[];
         for ( var i=0;i<data.data.length;i++)
         {
             a.push(data.data[i].country_or_area);
+            b.push(data.data[i].category);
         }
         a = a.filter(function(item, pos) {
             return a.indexOf(item) == pos; //filters the duplicate values
         })
+        b = b.filter(function(item, pos) {
+            return b.indexOf(item) == pos; //filters the duplicate values
+        })
+        setcountryList(a);
+        setcategoryList(b);
+        console.log('countrylist',categoryList);
     }
+    useEffect(() => {
+        console.log('data',data.data);
+        country();
+        console.log('mount it!');
+    }, []); 
     return(
         <div id="search">
         <br></br>
@@ -24,12 +39,12 @@ const Graph=()=>{
    <Col span={8}>
    <center><Card title="Input Country and Parameter" style={{ width: 300 }}>
    <Select style={{ width: 200 }} placeholder='Select Country' showSearch>
-      <Option value="jack">Jack</Option>
-      <Option value="lucy">Lucy</Option>
-      <Option value="disabled" disabled>
-        Disabled
-      </Option>
-      <Option value="Yiminghe">yiminghe</Option>
+       {countryList.map(country=> <Option key={country} value={country}>{country}</Option>)}
+    </Select>
+    <br></br>
+    <br></br>
+    <Select style={{ width: 200 }} placeholder='Select Parameter(Category)' showSearch>
+       {categoryList.map(category=> <Option key={category} value={category}>{category}</Option>)}
     </Select></Card></center>
      </Col>
       
